01d05772df652b3cd7a4462af898ded0fd95aded
Utkarsh721 <utkarshrai2121>
2023-02-01 04:57:06 +0530

    Style React Task 10

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -127,19 +127,7 @@ const Login = (props) => {
         onBlur={validatePasswordHandler}
         >
          </Input>
-        <div
-          className={`${classes.control} ${passwordState.isValid === false ? classes.invalid : ''
-            }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        
         <div
           className={`${classes.control} ${collageValid === false ? classes.invalid : ''
             }`}
