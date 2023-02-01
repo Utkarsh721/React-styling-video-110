4c0734bf2af1b27378e01593e111f1cab0d11bb2
Utkarsh721 <utkarshrai2121>
2023-02-01 05:31:00 +0530

    styling react task11

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, useReducer,useContext } from 'react';
+import React, { 
+  useState, 
+  useEffect, 
+  useReducer,
+  useContext, 
+  useRef 
+} from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -45,6 +51,8 @@ const Login = (props) => {
     isValid: null
   });
   const authCtx=useContext(AuthContext);
+  const emailInputRef=useRef();
+  const passwordInputRef=useRef();
   const { isValid: emailIsValid} = emailState;
   const { isValid: passwordISValid} = passwordState;
 
@@ -100,13 +108,20 @@ const Login = (props) => {
   }
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value, enteredCollage);
+    if(formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value, enteredCollage);
+     } else if(!emailIsValid) {
+       emailInputRef.current.focus();
+     }else{
+       passwordInputRef.current.focus();
+     }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
        <Input 
+       ref={emailInputRef}
        id="email"
         label="E-Mail" 
         type="email" 
@@ -118,6 +133,7 @@ const Login = (props) => {
 
         </Input>
         <Input 
+        ref={passwordInputRef}
        id="password"
         label="password" 
         type="password" 
@@ -143,7 +159,7 @@ const Login = (props) => {
           </input>
         </div>
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn} >
             Login
           </Button>
         </div>
